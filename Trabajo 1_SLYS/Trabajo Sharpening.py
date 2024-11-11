544b812c3f861a0059ef963da64d4951c58991e2
mxavier11 <156433334+mxavier11@users.noreply.github.com>
2024-11-10 19:39:41 -0500

    Update Trabajo Sharpening.py
    a

--- Trabajo 1_SLYS/Trabajo Sharpening.py
+++ Trabajo 1_SLYS/Trabajo Sharpening.py
@@ -1,9 +1,10 @@
 from PIL import Image # type: ignore
 import numpy as np # type: ignore
+import matplotlib.pyplot as plt
 
 # Cargar la imagen en formato BMP
 
-image = Image.open("img_borrosa.bmp")
+image = Image.open("imagen.bmp").convert("L")  # Convertir a escala de grises
 image_array = np.array(image)  # Convertir la imagen a un array de NumPy para manipular sus píxeles
 
 # Definir el kernel de sharpening
@@ -15,7 +16,7 @@ sharpening_kernel = np.array([[0, -1, 0],
 output_image = np.zeros_like(image_array)
 
 # Obtener las dimensiones de la imagen y del kernel
-image_height, image_width, _ = image_array.shape  # Incluimos el canal de color
+image_height, image_width = image_array.shape  # Incluimos el canal de color
 kernel_size = sharpening_kernel.shape[0]  # Suponemos un kernel cuadrado
 offset = kernel_size // 2  # Para evitar los bordes (donde no cabe el kernel)
 
@@ -32,10 +33,19 @@ for i in range(offset, image_height - offset):
         output_image[i, j] = min(max(result, 0), 255)
 
 # Convertir la matriz de salida en una imagen
-output_image = Image.fromarray(output_image.astype(np.uint8))
+output_image = Image.fromarray(output_image)
 
 # Guardar la imagen procesada en formato BMP
 output_image.save("imagen_sharpened_color_manual.bmp")
 
-# Mostrar la imagen procesada
-output_image.show()
+# Mostrar la imagen original
+axes[0].imshow(image)
+axes[0].set_title("Imagen Original")
+axes[0].axis("off")
+
+# Mostrar la imagen con sharpening
+axes[1].imshow(output_image)
+axes[1].set_title("Imagen con Sharpening")
+axes[1].axis("off")
+
+plt.show()
